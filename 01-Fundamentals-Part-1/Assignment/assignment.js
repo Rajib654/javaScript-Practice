8bbc87e13a6a96d145ea9247a0bc0ef2a8104ed4
Rajib654 <77357162+Rajib654@users.noreply.github.com>
2021-07-24 10:44:34 +0600

    Create a New Folder called Assignment & put on two files called assignment.html,assignment.js

---
+++ 01-Fundamentals-Part-1/Assignment/assignment.js
@@ -0,0 +1,15 @@
+
+// Assignments
+let country = 'Bangladesh';
+let continent = 'Asia';
+let population = 200000000;
+console.log('Country = ' + country);
+console.log('Continent = ' + continent);
+console.log('Population = ' + population);
+
+// Assignment 2
+let isIsland = true;
+
+console.log(typeof isIsland);
+console.log(typeof country);
+console.log(typeof population)
